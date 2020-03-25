9a9b46f7165438cac1eaf1ac202b2a87fa2202a4
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2020-03-25 14:56:47 -0400

    Kept rest of PPM file as words
    Found out that each RGB value of each pixel is separated by a space anyway

--- src/PPM.hs
+++ src/PPM.hs
@@ -15,11 +15,10 @@ data Header = Header
   , maxColourVal :: Int
   } deriving Show
 
-parseHeader :: ByteString -> (Header,ByteString)
+parseHeader :: ByteString -> (Header,[ByteString])
 parseHeader raw = (header, rest)
   where
     stripComments = BC.unlines . map (BC.takeWhile (/= '#')) . BC.lines
-    (_:rawHeader, splitRest) = splitAt 4 $ BC.words $ stripComments raw
+    (_:rawHeader, rest) = splitAt 4 $ BC.words $ stripComments raw
     headerAsInts = map (fst . fromJust . BC.readInt) rawHeader
     header = Header (headerAsInts !! 0) (headerAsInts !! 1) (headerAsInts !! 2)
-    rest = BC.unwords splitRest
