a3da1475f9e054fe30e5be2c82a531f142452a11
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2020-04-29 14:49:33 -0400

    Began using Tasty
    Began using Tasty to structure unit tests, provide nicer output, and allow property tests to be used too

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,21 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Test.HUnit
+import Test.Tasty
+import Test.Tasty.HUnit
 
 import qualified Data.ByteString.Lazy.Char8 as BC
 
 import PPM
 
 
-tests = TestList
-  [ "Line breaks in source" ~:
-      TestList ["don't affect parsed value" ~: test_lineBreaksDontAffectParsedValue]
-  , "Comments in source" ~:
-      TestList
-        [ "don't affect parsed value" ~: test_commentsDontAffectParsedValue
-        , "don't affect parsed value (inline)" ~: test_commentsDontAffectParsedValueInline
-        , "don't affect parsed value (semi-inline)" ~: test_commentsDontAffectParsedValueSemiInline
-        ]
+tests = testGroup "All Tests" [unitTests]
+
+unitTests = testGroup "Unit Tests"
+  [ testGroup "Line breaks in source"
+      [ testCase "don't affect parsed value" test_lineBreaksDontAffectParsedValue ]
+  , testGroup "Comments in source"
+      [ testCase "don't affect parsed value" test_commentsDontAffectParsedValue
+      , testCase "don't affect parsed value (inline)" test_commentsDontAffectParsedValueInline
+      , testCase "don't affect parsed value (semi-inline)" test_commentsDontAffectParsedValueSemiInline
+      ]
   ]
 
 test_lineBreaksDontAffectParsedValue =
@@ -26,7 +28,7 @@ test_lineBreaksDontAffectParsedValue =
         , "0 255 0"
         ]
       inputWithoutLineBreaks = "P3 1 1 255 0 255 0"
-  in parsePPM inputWithLineBreaks ~?= parsePPM inputWithoutLineBreaks
+  in parsePPM inputWithLineBreaks @?= parsePPM inputWithoutLineBreaks
 
 test_commentsDontAffectParsedValue =
   let inputWithComments = BC.unlines
@@ -41,12 +43,12 @@ test_commentsDontAffectParsedValue =
         , "255"
         , "0 255 0"
         ]
-  in parsePPM inputWithComments ~?= parsePPM inputWithoutComments
+  in parsePPM inputWithComments @?= parsePPM inputWithoutComments
 
 test_commentsDontAffectParsedValueInline =
   let inputWithComments = "P3 1 1 255 0 255 0 # This ## is #a comment"
       inputWithoutComments = "P3 1 1 255 0 255 0"
-  in parsePPM inputWithComments ~?= parsePPM inputWithoutComments
+  in parsePPM inputWithComments @?= parsePPM inputWithoutComments
 
 test_commentsDontAffectParsedValueSemiInline =
   let inputWithComments = BC.unlines
@@ -57,9 +59,7 @@ test_commentsDontAffectParsedValueSemiInline =
         [ "P3 1 1 255"
         , "0 255 0"
         ]
-  in parsePPM inputWithComments ~?= parsePPM inputWithoutComments
+  in parsePPM inputWithComments @?= parsePPM inputWithoutComments
 
 main :: IO ()
-main = do
-  runTestTT tests
-  return ()
+main = defaultMain tests
